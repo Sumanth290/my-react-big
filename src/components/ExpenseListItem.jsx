2e309b1226db7dd1fddfc7bdeb4efba1c1b42549
Sumanth290 <sumanth.hema@gmail.com>
2018-05-14 09:07:55 -0500

    added custom amt format

--- src/components/ExpenseListItem.jsx
+++ src/components/ExpenseListItem.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import moment from "moment";
-
+import myFormat from "../util/amtFormatter";
 
 export const ExpenseListItem =  ({desc,note,amount,timestamp,id}) => (
     <div>
         <h3>Description : <Link to={`/edit/${id}`}>{desc}</Link></h3>
         { note && <p>Note : {note}</p> }
-        <p>Amount : {amount} &cent;</p>
+        {/* <p>Amount : {numeral(amount/100).format("$0,0.00")}</p> */}
+        <p>Amount : ₹ {myFormat(amount/100)}</p>
         <p>Date : {moment(timestamp).format("MMM DD, Y")}</p>
     </div>
 );
+
 
 export default ExpenseListItem;
