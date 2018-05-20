49c7d3ba45f82845333118a9fd16fa45c8468ea6
Sumanth290 <sumanth.hema@gmail.com>
2018-05-20 11:32:14 -0500

    corrected spelling

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
+console.log("process.env.NODE_ENV",process.env.NODE_ENV);
+
 if(process.env.NODE_ENV === "test"){
   require('dotenv').config({path : ".env.test"});
 }else if(process.env.NODE_ENV === "development"){
@@ -50,12 +52,12 @@ module.exports = (env,argv) => {
     plugins : [
       CSSExtract,
       new webpack.DefinePlugin({
-        "process.ENV.FIREBASE_API_KEY" : JSON.stringify(process.ENV.FIREBASE_API_KEY),
-        "process.ENV.FIREBASE_AUTH_DOMAIN" : JSON.stringify(process.ENV.FIREBASE_AUTH_DOMAIN),
-        "process.ENV.FIREBASE_DATABASE_URL" : JSON.stringify(process.ENV.FIREBASE_DATABASE_URL),
-        "process.ENV.FIREBASE_PROJECT_ID" : JSON.stringify(process.ENV.FIREBASE_PROJECT_ID),
-        "process.ENV.FIREBASE_STORAGE_BUCKET" : JSON.stringify(process.ENV.FIREBASE_STORAGE_BUCKET),
-        "process.ENV.FIREBASE_MESSAGING_SENDER_ID" : JSON.stringify(process.ENV.FIREBASE_MESSAGING_SENDER_ID)
+        "process.env.FIREBASE_API_KEY" : JSON.stringify(process.env.FIREBASE_API_KEY),
+        "process.env.FIREBASE_AUTH_DOMAIN" : JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        "process.env.FIREBASE_DATABASE_URL" : JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        "process.env.FIREBASE_PROJECT_ID" : JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        "process.env.FIREBASE_STORAGE_BUCKET" : JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        "process.env.FIREBASE_MESSAGING_SENDER_ID" : JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
       })
     ],
     devtool : isProd ? "source-map" : "inline-source-map",
