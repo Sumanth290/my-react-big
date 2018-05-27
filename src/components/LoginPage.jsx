0ae37ac1ed3519727afd3281240f5cf893decf46
Sumanth290 <sumanth.hema@gmail.com>
2018-05-27 16:48:28 -0500

    Initial styling login page

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -3,9 +3,17 @@ import {connect} from "react-redux";
 import {startLogin} from "../actions/auth";
 
 export const LoginPage = (props) => (
-    <div>
-        <h1>Please login to continue</h1>
-        <button onClick={props.startLogin}>Login</button>
+    <div className="box-layout">
+        {/* <h1>Please login to continue</h1> */}
+        <div className="box-layout__fade">
+            <div className="box-layout__title">
+                <p className="box-layout__heading">Expensify</p>
+                <p className="box-layout__text">It's time to get your expenses under control</p>
+            </div>
+            <div className="box-layout__box">
+                <button className="box-layout__button" onClick={props.startLogin}>Login with Google</button>
+            </div>
+        </div>
     </div>
 );
 
